1501c169d52b7775f71b0dcd788fd85889e6b04d
Otavio Jacobi <otaviojacobi@gmail.com>
2025-05-20 12:46:34 -0300

    Add basic project skeleton
    Change-type: patch

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,17 @@
-import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { setFailed } from '@actions/core';
+import { context } from '@actions/github';
+import { getInputs } from './input.js';
 
-/**
- * The main function for the action.
- *
- * @returns Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+	try {
+		const inputs = await getInputs();
+		console.log('The input values are', JSON.stringify(inputs));
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+		// Get the JSON webhook payload for the event that triggered the workflow
+		// TODO: remove me
+		const payload = JSON.stringify(context.payload, undefined, 2);
+		console.log(`The event payload: ${payload}`);
+	} catch (error) {
+		setFailed(error.message);
+	}
 }
